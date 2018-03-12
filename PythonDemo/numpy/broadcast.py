fc0f6512bccd0f5b594f1f6b526ada2c937cded3
Snaiclimb <koushuangbwcx@163.com>
2018-03-12 16:49:17 +0800

    numpy

---
+++ PythonDemo/numpy/broadcast.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Mar 12 16:29:45 2018
+
+@author: Administrator
+"""
+import numpy as np
+a=np.array([[[1, 2, 3], [4, 5, 6]], [[7, 8, 9], [10, 11, 12]]])
+b=np.array([1, 2, 3])
+c=a+b
+print(c)
